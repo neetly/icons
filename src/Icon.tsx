76ff8486a155cca82e889025de7be4e319402f4f
Hikari Hayashi <rev.hikari@gmail.com>
2022-12-31 23:31:56 +0800

    Update

---
+++ src/Icon.tsx
@@ -0,0 +1,7 @@
+import type { ComponentType, SVGAttributes } from "react";
+
+type IconType = ComponentType<IconProps>;
+
+type IconProps = SVGAttributes<SVGSVGElement>;
+
+export type { IconProps, IconType };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export {};
+export * from "./Icon";
+export * from "./icons";
